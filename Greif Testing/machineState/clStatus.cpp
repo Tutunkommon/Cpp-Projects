aa03d9b5cc0afa6f0c1abaf6cde3cefd905cd470
Tutunkommon <jjansen@seltrol.com>
2022-05-19 14:56:03 -0400

    words

---
+++ Greif Testing/machineState/clStatus.cpp
@@ -0,0 +1,21 @@
+class clStatus{	
+
+	private:
+		int state;
+		int speed;
+		int beltFeet;
+
+	public:
+		int setState(int newState){
+			//  TO DO:  Report new state
+		}
+
+		int setSpeed(int newSpeed){
+			// TO DO:  Update machine speed
+		}
+
+		int setBeltFeet(int newBeltFeet){
+			// TO DO:  Update current belt feet reporting
+		}
+
+}

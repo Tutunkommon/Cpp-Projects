0a2cb5192d226b60eb97e4c05fb834e95f83cd6c
Tutunkommon <joe.jansen@gmail.com>
2022-05-31 19:30:09 +0000

    It finally compiles!

--- Greif Testing/winder/src/clsSpiralIO.h
+++ Greif Testing/winder/src/clsSpiralIO.h
@@ -1,8 +1,4 @@
 
-#ifndef modbus
-#define modbus
-#include "modbuspp/modbus.h"
-#endif
 
 
 
@@ -18,38 +14,36 @@ class clsSpiralIO{
 		unsigned short totalCountLo;
 		unsigned short totalCountHi;
 
-		modbus mb;
-
 
 
 	public:
 
-		bool  :  restartEnable;			//412289.10
-		bool  :  bottomBypass;			//412289.11
-		bool  :  estopOK;				//412289.12
-		bool  :  resetCount;			//412289.13
-		bool  :  FPMDummy;				//412289.14
-		bool  :  jogFestoonFWD;			//412290.0
-		bool  :  jogFestoonREV;			//412290.1
-		bool  :  jogHarpFWD;			//412290.2
-		bool  :  jogHarpREV;			//412290.3
-		bool  :  jogOpen;				//412290.4
-		bool  :  jogClose;				//412290.5
-		long  :  totalCount;			//412291.LONG
-		long  :  speed;					//412293.LONG
-		long  :  dummySpeed;			//412295.LONG
-		bool  :  supressEncoder;		//400001.4
-		bool  :  markerAirTaylorsFull;	//400518.10
-		bool  :  markerInkTaylorsFull;	//400518.11
-		long  :  totalFeet;				//totalCount / 12
-		bool  :  tubeMarkerAir;			//000515
-		bool  :  tubeMarkerInk;			//000516
-		bool  :  horn;					//000517
+		bool    restartEnable;			//412289.10
+		bool    bottomBypass;			//412289.11
+		bool    estopOK;				//412289.12
+		bool    resetCount;			//412289.13
+		bool    FPMDummy;				//412289.14
+		bool    jogFestoonFWD;			//412290.0
+		bool    jogFestoonREV;			//412290.1
+		bool    jogHarpFWD;			//412290.2
+		bool    jogHarpREV;			//412290.3
+		bool    jogOpen;				//412290.4
+		bool    jogClose;				//412290.5
+		long    totalCount;			//412291.LONG
+		long    speed;					//412293.LONG
+		long    dummySpeed;			//412295.LONG
+		bool    supressEncoder;		//400001.4
+		bool    markerAirTaylorsFull;	//400518.10
+		bool    markerInkTaylorsFull;	//400518.11
+		long    totalFeet;				//totalCount / 12
+		bool    tubeMarkerAir;			//000515
+		bool    tubeMarkerInk;			//000516
+		bool    horn;					//000517
 
 
-		clsSpiralIO(char* ipAddress){
+		clsSpiralIO(){
+			modbus mb = modbus("192.168.101.5", 502);
 
-			mb = modbus(ipAddress, 502);
 			mb.modbus_set_slave_id(1);
 			mb.modbus_connect();
 		};
@@ -64,7 +58,7 @@ class clsSpiralIO{
 		};
 
 		bool getEstopState(){
-            return(estopOK);
+	            return(estopOK);
 		};
 
 		void setResetCount(bool val){
@@ -76,11 +70,8 @@ class clsSpiralIO{
 		};
 
 		void setJogFestoonFWD(bool val){
-            jogFestoonFWD = val;
+        	    jogFestoonFWD = val;
 		};
-
-
-
 
 };
 
--- Greif Testing/winder/src/winder.cpp
+++ Greif Testing/winder/src/winder.cpp
@@ -1,12 +1,14 @@
 
 
-#include "clSpiralIO.h"
-
+#include "modbus.h"
+#include "clsSpiralIO.h"
 
 #include <chrono>
 #include <thread>
 #include <cstdio>
+#include <iostream>
 
+using namespace std;
 
 
 int main(int argc, char **argv){
@@ -14,20 +16,15 @@ int main(int argc, char **argv){
 	using namespace std::this_thread;
 	using namespace std::chrono;
 
-	clsSpiralIO line1("192.168.101.1");
+	clsSpiralIO line1;
 
 	int x;
 	uint16_t read_holding_regs[1];
 
-	while(x < 50){
-			mb.modbus_read_holding_registers(0,2,read_holding_regs);
 
-			if(read_holding_regs[0] > 0){
-					x++;
-					status.setBeltFeet(x);
-			}
-			sleep_for(seconds(1));
-	}
+	cout << line1.getEstopState();
+
+
 	return(0);
 }
 
